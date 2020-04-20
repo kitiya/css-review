926faa3a4d27c7fa3a97d5256370346a090698b0
Kitiya <kate.kitiya@gmail.com>
2020-04-20 03:47:04 -0400

    Updates

---
+++ precache-manifest.54fe995c08afbf9717be0a311aa31426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16132dd81084330053c50f7a315175c8",
+    "url": "/css-review/index.html"
+  },
+  {
+    "revision": "624c8f22477d854f8a7c",
+    "url": "/css-review/static/css/main.e3af9821.chunk.css"
+  },
+  {
+    "revision": "74c7c7fb2b6fc2d6cab9",
+    "url": "/css-review/static/js/2.9b42f315.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/css-review/static/js/2.9b42f315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624c8f22477d854f8a7c",
+    "url": "/css-review/static/js/main.20d1402a.chunk.js"
+  },
+  {
+    "revision": "943bc5981038ca6097b7",
+    "url": "/css-review/static/js/runtime-main.f472abeb.js"
+  }
+]);

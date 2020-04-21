cacbf5313341f720aa5379f08b2e8981439abac1
Kitiya <kate.kitiya@gmail.com>
2020-04-21 06:24:02 -0400

    Challenge#3:solution

--- src/pages/responsive-layouts/challenge-03/challenge-03.jsx
+++ src/pages/responsive-layouts/challenge-03/challenge-03.jsx
@@ -7,19 +7,23 @@ const Challenge03 = () => {
     <>
       <section className="challenge-sandbox" id="ch02">
         <div className="body03">
-          <div className="container">
-            <h2 className="title">
-              Responsive layouts don't have to be a struggle
-            </h2>
-            <p className="subtitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem aspernatur distinctio laudantium dolores. Nulla
-              quibusdam reprehenderit eum sit minus aliquid! Exercitationem
-              aspernatur distinctio laudantium dolores.
-            </p>
-            <a href="#" className="btn">
-              I want to learn
-            </a>
+          <div className="hero">
+            <div className="container">
+              <div className="hero__text">
+                <h2 className="title">
+                  Responsive layouts don't have to be a struggle
+                </h2>
+                <p className="subtitle">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Exercitationem aspernatur distinctio laudantium dolores. Nulla
+                  quibusdam reprehenderit eum sit minus aliquid! Exercitationem
+                  aspernatur distinctio laudantium dolores.
+                </p>
+                <a href="#" className="btn">
+                  I want to learn
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
